de6fcb357af61b2cba4245fddbef4ec22010e9a8
Pavel Korchagin <mortalis94@gmail.com>
2020-04-28 18:56:11 +0300

    Реализован метод сортировки Вставками, а также написаны тесты к нему

--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -3,7 +3,7 @@ from typing import Any, List
 
 import pytest
 
-from src.sorts import selection_sort, bubble_sort
+from src.sorts import bubble_sort, insertion_sort, selection_sort
 
 test_data = [
     [5, 1, 3, 2, 6, 4],
@@ -40,4 +40,18 @@ def test_bubble_sort(arr: List[Any]) -> None:
     """
     dump = deepcopy(arr)
     assert bubble_sort(arr) == sorted(arr)
+    assert arr == dump
+
+
+@pytest.mark.parametrize('arr', test_data)
+def test_insertion_sort(arr: List[Any]) -> None:
+    """
+    Тестирование метода сортировки Вставками.
+
+    Проверяет также на неизменность исходного массива.
+
+    :param arr: Исходный массив элементов
+    """
+    dump = deepcopy(arr)
+    assert insertion_sort(arr) == sorted(arr)
     assert arr == dump
