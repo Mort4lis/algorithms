b36d76ab7f90204c9f1822c3377c3e9b197ba237
Pavel Korchagin <mortalis94@gmail.com>
2020-04-30 17:47:36 +0300

    Реализован и протестирован рекурсивный метод бинарного поиска

--- src/search.py
+++ src/search.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+from typing import Any, List
 
 
 def binary_search(arr: List[Any], item: Any) -> int:
@@ -28,3 +28,26 @@ def binary_search(arr: List[Any], item: Any) -> int:
         else:
             end = mid - 1
     return -1
+
+
+def recursive_binary_search(arr: List[Any], item: Any, begin=0, end=None) -> int:
+    """
+    Метод рекурсивного бинарного поиска.
+
+    :param arr: Отсорированный массив элементов
+    :param item: Искомый элемент
+    :param begin: Начальный индекс рассматриваемой задачи
+    :param end: Конечный индекс рассматриваемой задачи
+    :return: Возвращает индекс найденного элемента, либо -1 в случае отсутствия элемента в массиве
+    """
+    end = len(arr) - 1 if end is None else end
+    if begin >= end:
+        return -1
+
+    mid = (begin + end) // 2
+    if arr[mid] == item:
+        return mid
+
+    if arr[mid] < item:
+        return recursive_binary_search(arr, item, begin=mid + 1, end=end)
+    return recursive_binary_search(arr, item, begin=begin, end=mid)
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,23 +1,44 @@
+from typing import Any, List
+
 import pytest
-from typing import List, Any
 
-from src.search import binary_search
+from src.search import binary_search, recursive_binary_search
+
+test_data = [
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 8),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 11),
+    ([], 10),
+    ([-5, -3, 5, 10, 15], -3),
+    ([1, 3, 5, 7, 9], 3),
+    ([1, 3, 5, 7, 9], -1)
+]
 
 
-@pytest.mark.parametrize(['arr', 'item', 'result'], [
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 8, 7),
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 11, -1),
-    ([], 10, -1),
-    ([-5, -3, 5, 10, 15], -3, 1),
-    ([1, 3, 5, 7, 9], 3, 1),
-    ([1, 3, 5, 7, 9], -1, -1)
-])
-def test_binary_search(arr: List[Any], item: Any, result: int) -> None:
+@pytest.mark.parametrize(['arr', 'item'], test_data)
+def test_binary_search(arr: List[Any], item: Any) -> None:
     """
     Тестирование метода Бинарного поиска.
 
     :param arr: Отсорированный массив элементов
     :param item: Искомый элемент
-    :param result: Индекс искомого элемента
     """
-    assert binary_search(arr, item) == result
+    try:
+        index = arr.index(item)
+    except ValueError:
+        index = -1
+    assert binary_search(arr, item) == index
+
+
+@pytest.mark.parametrize(['arr', 'item'], test_data)
+def test_recursive_binary_search(arr: List[Any], item: Any) -> None:
+    """
+    Тестирование метода Рекурсивного Бинарного поиска.
+
+    :param arr: Отсорированный массив элементов
+    :param item: Искомый элемент
+    """
+    try:
+        index = arr.index(item)
+    except ValueError:
+        index = -1
+    assert recursive_binary_search(arr, item) == index
